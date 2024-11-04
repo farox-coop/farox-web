83b4c64d9388c1155a3b39b07e449517d625564b
Lucas Ferraro <lucasf@farox.coop>
2024-11-04 14:45:27 -0300

    remove build folder

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/home/tincho/dev/farox-web/app/[locale]/layout":["static/media/6d6d0e4a023f9f33-s.p.woff2","static/media/791334aa8b2b679f-s.p.woff2","static/media/f9946f06d70a1b5b-s.p.woff2","static/media/08404bcfb1dae67a-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';

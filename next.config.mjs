f1e00af4735f8f7ab6bbce7e780d6ed08cb31faf
Lucas Ferraro <lucasf@farox.coop>
2024-11-04 13:19:35 -0300

    revert basepath remove

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,8 @@ const withNextIntl = createNextIntlPlugin()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/farox-web',
+  assetPrefix: '/farox-web/',
 }
 
 export default withNextIntl(nextConfig)

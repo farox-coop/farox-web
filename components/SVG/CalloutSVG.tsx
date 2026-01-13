cb28908c7b7fa6090912f3790e861e8e943524b1
Hugo Avila <hugok2k@gmail.com>
2026-01-13 09:31:01 -0300

    feat: add CalloutSVG component for enhanced visual elements

---
+++ components/SVG/CalloutSVG.tsx
@@ -0,0 +1,11 @@
+export default function CalloutSVG() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="54" height="48" fill="none">
+      <path fill="#6843E1" d="M54 0H0v48h54z" />
+      <path
+        fill="#fff"
+        d="M38.062 36.125H16.713a.713.713 0 0 1 0-1.426h19.725l-18.59-20.206a.712.712 0 1 1 1.049-.965l19.689 21.4a.714.714 0 0 1-.524 1.197m0-6.272a.713.713 0 0 1-.713-.713V11.713a.713.713 0 1 1 1.426 0V29.14c0 .394-.32.713-.713.713"
+      />
+    </svg>
+  )
+}
